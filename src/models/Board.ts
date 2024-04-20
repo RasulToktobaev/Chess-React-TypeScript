6e5ffd54057712c74269fb4f9a3a853e8985cd3a
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-04-20 12:49:11 +0600

    initialization of chessboard cells

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,4 +1,5 @@
 import {Cell} from "./cell";
+import {Colors} from "./Colors";
 
 export class Board {
     cells: Cell[][] = []
@@ -7,10 +8,13 @@ export class Board {
         for (let i = 0; i < 8; i++) {
             const row: Cell[] = []
             for (let j = 0; j < 8; j++) {
-                if((i + j) % 2 !== 0){
-                    row.push(new Cell()) //Черные ячейки
+                if ((i + j) % 2 !== 0) {
+                    row.push(new Cell(this, j, i, Colors.BLACK, null)) //Черные ячейки
+                } else {
+                    row.push(new Cell(this, j, i, Colors.WHITE, null)) //Белые ячейки
                 }
             }
+            this.cells.push(row);
         }
     }
 }
