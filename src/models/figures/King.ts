e360e334f06498ffd8f24fcb4cc882c7db3d0c5d
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-04-23 20:20:18 +0600

    logic for king

--- src/models/figures/King.ts
+++ src/models/figures/King.ts
@@ -10,9 +10,21 @@ export class King extends Figure {
         this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
         this.name = FigureNames.KING;
     }
+
     canMove(target: Cell): boolean {
-        if(!super.canMove(target))
+        if (!super.canMove(target))
             return false;
-        return true
+        const Dx = Math.abs(this.cell.x - target.x);
+        const Dy = Math.abs(this.cell.y - target.y);
+
+        if ((Dx <= 1 && Dy <= 1) || (Dx === 1 && Dy === 1)) {
+
+            if (target.figure && target.figure.color === this.color) {
+                return false;
+            }
+            return true;
+        }
+        return false
     }
 }
+
